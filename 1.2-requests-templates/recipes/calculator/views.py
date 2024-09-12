cc295c4a63388ea536c3194e1a25d7c66fd4ecb6
VeniVidiVigor <142258002+VeniVidiVigor@users.noreply.github.com>
2024-09-12 13:22:23 +0300

    1.2-requests-templates
    1.2-requests-templates

---
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def all_recipes_view(request, recipe_name):
+    if recipe_name in DATA.keys():
+        context = {
+            'recipe': DATA[recipe_name].copy()
+        }
+
+        if request.GET.get('servings'):
+            servings = int(request.GET.get('servings'))
+
+            for ingridient, count in context['recipe'].items():
+                context['recipe'][ingridient] = DATA[recipe_name][ingridient] * servings
+
+        return render(request, 'index.html', context=context)
+
